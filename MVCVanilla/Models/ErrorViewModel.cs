0f7f77f007bdddd48fcab201ca033dc5eac4fa13
DustyDaze <globalartichoke@googlemail.com>
2020-01-15 14:58:07 +0000

    Initial commit

---
+++ MVCVanilla/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCVanilla.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
